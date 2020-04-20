5d5b55a76d3c3adcea0b64af567c80b0cd32c3d7
Administrator <yuankun.shang@globetools.com>
2020-04-20 16:36:43 +0800

    add clean-webpack-plugin webpack-bundle-analyzer webpack-jarvis

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,10 @@ const {
   addWebpackPlugin,
 } = require('customize-cra')
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const Jarvis = require('webpack-jarvis')
 
 module.exports = override(
   fixBabelImports('import', {
@@ -16,5 +20,19 @@ module.exports = override(
     javascriptEnabled: true,
     modifyVars: { '@primary-color': '#1DA57A' },
   }),
-  addWebpackPlugin(new AntdDayjsWebpackPlugin())
+  addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+  addWebpackPlugin(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'report.html',
+      openAnalyzer: false,
+      generateStatsFile: true,
+    })
+  ),
+  addWebpackPlugin(new CleanWebpackPlugin())
+  // addWebpackPlugin(
+  //   new Jarvis({
+  //     port: 1337,
+  //   })
+  // )
 )
--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Layout, Menu, Breadcrumb } from 'antd'
 import './App.css'
+
+const { Header, Footer, Content } = Layout
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" />
+        </Header>
+        <Content
+          className="site-layout"
+          style={{ padding: '0 50px', marginTop: 64 }}
+        >
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item></Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 380 }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     )
   }
 }
